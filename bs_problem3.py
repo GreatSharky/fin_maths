875ed9698605f7a2ffebdf409dcb33ecc602f499
Aaro Haikonen <aaro.haikonen99@gmail.com>
2025-03-02 21:35:18 +0200

    completed part a

--- bs_problem3.py
+++ bs_problem3.py
@@ -3,7 +3,7 @@ import scipy as sp
 import matplotlib.pyplot as plt
 
 def stock_sim(stock, time, mu, sigma):
-    epsilon = np.random.norm(0,1)
+    epsilon = np.random.normal(0,1)
     return stock*np.exp((mu-1/2*sigma**2)*time + sigma*epsilon*np.sqrt(time)), epsilon
 
 def bs_formula(stock, strike, time, riskless_rate, volatility):
@@ -12,3 +12,29 @@ def bs_formula(stock, strike, time, riskless_rate, volatility):
     cdf = sp.stats.norm.cdf
     return stock*cdf(d1) - strike*np.exp(-riskless_rate*time)*cdf(d2)
 
+def a():
+    dt = 1/252
+    T = 1
+    N = int(1/dt)
+    S0 = 50
+    mu = .15
+    sigma = .5
+    K = 50
+    q = 0
+    r = .13
+    stocks = [S0]
+    epsilons = []
+    calls = []
+    for i in range(N):
+        time = i*dt
+        stock, epsilon = stock_sim(stocks[i], dt, mu, sigma)
+        stocks.append(stock)
+        epsilons.append(epsilon)
+        call_price = bs_formula(stock, strike=K, time=1-time, riskless_rate=r, volatility=sigma)
+        calls.append(call_price)
+    
+    fig, ax = plt.subplots()
+    ax.plot(np.arange(N), stocks[1:], np.arange(N), calls, np.arange(N), K*np.ones(N))
+    plt.show()
+
+a()
